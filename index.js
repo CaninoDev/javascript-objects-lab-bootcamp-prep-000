5fc99006286f9bef99f10ed2b6bbe98566e029ac
Ian Canino <ian.canino@gmail.com>
2018-03-23 04:01:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,18 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return newObj
 }
 
-funciton 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
 
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
